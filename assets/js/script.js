9727d2dc2a89d9c41b465ff2de1d61de44d234a5
lucijahajdu <lucija.hajdu@gmail.com>
2023-02-01 22:42:16 +0000

    Add comments for necessary steps in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Assign the buttons to variable
+// When the user clicks the button, we need to get the value of the button
+// Let the computer guess
+// Compare the players and computers choice
+// If computer wins, add to the computers score
+// If player wins, add to the players score
+// If it is a tie we do nothing
+// Update the score
+// If player has a score 10 we have a winner
+// Allow the user to reset the game
