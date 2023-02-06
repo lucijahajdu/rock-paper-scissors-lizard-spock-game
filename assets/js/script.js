7b300ce0de61af362ef10f1040c6713b1ccf4915
lucijahajdu <lucija.hajdu@gmail.com>
2023-02-06 22:11:45 +0000

    Correct update score area and display modal result

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,8 @@ const outcome = document.getElementById('outcome');
 const icon = document.getElementById('icon');
 const computerOutcome = document.getElementById('computerHand');
 const restartButton = document.getElementById('restart');
-
 const score = document.getElementById('scoreArea');
-
-let playerScore = 0;
-let computerScore = 0;
-
-
+const resultContainer = document.getElementById('resultContainer');
 const hands = [{
     name: 'rock',
     beats: ['scissors', 'lizard'],
@@ -40,41 +35,39 @@ const hands = [{
     icon: 'fa-solid fa-hand-spock'
 }
 ];
-// When the user clicks the button, we need to get the value of the button
 
+let playerScore = 0;
+let computerScore = 0;
 
-
-
+// Add event listener to each game button
 for (let button of buttons) {
-    button.addEventListener('click', function(){
+    button.addEventListener('click', function() {
+        console.log(playerScore)
         let playerGuess = hands.find((hand)=> hand.name === button.value);
         let computerGuess = getComputerGuess();
-        console.log(playerGuess.name, computerGuess.name);
         let compareGuess = compare(playerGuess, computerGuess);
         
-        
-
-        setResult=(playerGuess.icon, computerGuess.hand, compareGuess);
-        updateScore(compareGuess);
-        console.log(playerScore, computerScore);
+        setResult = (playerGuess.icon, computerGuess.hand, compareGuess);
+        updateScore(compareGuess, computerGuess);
         if(playerScore === 10 || computerScore === 10) {
-          alert('Game Over')
+          let winner = playerScore === 10 ? 'player' : 'computer';
+          setScore()
+          setTimeout(() => {
+            alert(`Game over! ${winner} wins!`)
+            restartGame();
+          }, 500);
         }
         
     });
 };
 
-
-// Let the computer guess
-
+// Let the computer guess a random hand
 function getComputerGuess() {
     const computerHand = hands[Math.floor(Math.random() * 5)]
 
     return computerHand
-
     }
     
-
 // Compare the players and computers choice
 
 function compare(playerGuess, computerGuess) {
@@ -91,38 +84,35 @@ function compare(playerGuess, computerGuess) {
     return result;
 };
 
-
-
 // If computer wins, add to the computers score
 // If player wins, add to the players score
 // If it is a tie we do nothing
 // Update the score
-
-
-function updateScore(winner) {
+function updateScore(winner, computerGuess) {
   if (winner === 'player') {
-  
-    playerScoreDisplay.innerText = playerScore++;
+    playerScore++;
     // Show modal result
     resultArea.innerHTML = `
     <h1 class="text-win">You win!</h1>
-    <i class="fa-solid fa-hand-back-${getComputerGuess}" ></i>
-    <p>Computer chose <strong>${getComputerGuess}</strong></p>`
+    <i class="fa-solid fa-hand-back-${computerGuess.icon}" ></i>
+    <p>Computer chose <strong>${computerGuess.name}</strong></p>`
   } else if (winner === 'computer') {
     computerScore++;
-    computerScoreDisplay.innerText = computerScore;
     resultArea.innerHTML = `
     <h1 class="text-lose">You lose!</h1>
-    <i class="fa-solid fa-hand-back-${getComputerGuess}" ></i>
-    <p>Computer chose <strong>${getComputerGuess}</strong></p>`
+    <i class="fa-solid fa-hand-back-${computerGuess.icon}" ></i>
+    <p>Computer chose <strong>${computerGuess.name}</strong></p>`
   } else {
     resultArea.innerHTML = `
     <h1 class="text-lose">It's a draw!!</h1>
-    <i class="fa-solid fa-hand-back-${getComputerGuess}" ></i>
-    <p>Computer chose <strong>${getComputerGuess}</strong></p>`
-
+    <i class="fa-solid fa-hand-back-${computerGuess.icon}" ></i>
+    <p>Computer chose <strong>${computerGuess.name}</strong></p>`
   }
-  
+  setScore()
+  displayRoundResult()
+  setTimeout(() => {
+    displayRoundResult()
+  }, 2000);
 }
 
 // Set the score
@@ -131,6 +121,10 @@ function setScore() {
   computerScoreDisplay.innerHTML = computerScore;
 }
 
+// Display the result of the round
+function displayRoundResult() {
+  resultContainer.classList.contains('hide') ? resultContainer.classList.remove('hide') : resultContainer.classList.add('hide');
+}
 
 // If player has a score 10 we have a winner
 // Allow the user to reset the game
